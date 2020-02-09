08b93653e0b9cbd425fc08056bc65a362d146254
rupesh <rupesh.bende@games24x7.com>
2020-02-09 12:32:57 +0530

    Added properties to member object

--- src/main/java/com/broadcom/userservice/beans/Privilege.java
+++ src/main/java/com/broadcom/userservice/beans/Privilege.java
@@ -1,5 +1,10 @@
 package com.broadcom.userservice.beans;
 
 public class Privilege {
-
+	int id;
+	String name;
+	
+	public Privilege(String name) {
+		this.name = name;
+	}
 }
--- src/main/java/com/broadcom/userservice/beans/User.java
+++ src/main/java/com/broadcom/userservice/beans/User.java
@@ -1,5 +1,18 @@
 package com.broadcom.userservice.beans;
 
 public class User {
-
+	long id;
+	String firstName;
+	String lastName;
+	String email;
+	
+	public User(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	public User(String email) {
+		this.email = email;
+	}
 }
